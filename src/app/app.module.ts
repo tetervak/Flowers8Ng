0c73f2efe8f5988c510adb1071395b1f187454d3
Alex Tetervak <tetervak@sheridancollege.ca>
2019-12-05 19:46:30 -0500

    removed FlowerDetailsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {FlowerDetailsComponent} from './flower-details/flower-details.component';
-import {FlexLayoutModule} from '@angular/flex-layout';
 import {FlowerDataService} from './flower-data.service';
 import {FooterModule} from './footer/footer.module';
 import {HttpClientModule} from '@angular/common/http';
@@ -11,13 +9,11 @@ import { FlowerTableComponent } from './flower-table/flower-table.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FlowerDetailsComponent,
     FlowerTableComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FlexLayoutModule,
     FooterModule
   ],
   providers: [FlowerDataService],
