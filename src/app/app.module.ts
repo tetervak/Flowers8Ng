5dfcc1464db3405e597be73d4fea4edfec106201
Alex Tetervak <tetervak@sheridancollege.ca>
2019-12-05 19:42:02 -0500

    made the table work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {FlowerDetailsComponent} from './flower-details/flower-details.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FlowerDataService} from './flower-data.service';
+import {FooterModule} from './footer/footer.module';
+import {HttpClientModule} from '@angular/common/http';
+import { FlowerTableComponent } from './flower-table/flower-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlowerDetailsComponent,
+    FlowerTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    FooterModule
   ],
-  providers: [],
+  providers: [FlowerDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
